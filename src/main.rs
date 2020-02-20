edb2cd04ee66091510444b5b43adba415154b61c
ajbt200128 <austin@cutedogs.org>
2020-02-20 15:27:44 -0500

    Added templating example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::sync::Arc;
+
 use warp::Filter;
-use warp::Rejection;
-use warp::Reply;
+
+use lazy_static::lazy_static;
+
+use tera::Tera;
 
 mod util;
 
@@ -8,8 +12,25 @@ mod handlers;
 mod routes;
 mod models;
 
+lazy_static! {
+    pub(crate) static ref TEMPLATES: Arc<Tera> = {
+        let mut tera = match Tera::new("templates/**/*") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        if cfg!(feature = "autoreload"){
+            tera.full_reload().unwrap();
+        }
+        Arc::new(tera)
+    };
+}
+
 #[tokio::main]
 async fn main() {
+
     util::tracing::start_tracing();
     let routes = routes::routes().with(warp::trace::request());
     util::starter::start_server(routes).await;
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,19 @@
-use warp::{Filter, Rejection, Reply};
+use std::convert::Infallible;
+use std::sync::Arc;
+
+use tera::Context;
+use tera::Tera;
+use crate::TEMPLATES;
+use warp::{Filter, Rejection, Reply, reply::html};
 
 pub fn routes() -> impl Filter<Extract = (impl Reply,), Error= Rejection> + Clone + Send + Sync + 'static {
+    warp::path::end().and(with_templates()).map(|templates:Arc<Tera>| {
+        let mut context = Context::new();
+        context.insert("number", &2);
+        html(templates.render("index.html", &context).unwrap())
+    })
+}
+
+pub fn with_templates() -> impl Filter<Extract = (Arc<Tera>,), Error = Infallible>  + Clone + Send + Sync + 'static {
+    warp::any().map(move || TEMPLATES.clone())
 }
