46efcfe46ae5247e8491794049d442865ac97e29
ajbt200128 <austin@cutedogs.org>
2020-02-20 15:27:44 -0500

    Simple example setup using Postgres

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use warp::Filter;
-use warp::Rejection;
-use warp::Reply;
+use refinery::config::Config;
+use sqlx::postgres::PgPool;
 
 mod util;
-
 mod handlers;
 mod routes;
 mod models;
+mod migrations;
 
 #[tokio::main]
 async fn main() {
+
+    let mut refinery_config = Config::from_env_var("DB_URL").unwrap();
+    migrations::runner().run_async(&mut refinery_config).await.unwrap();
+    let pool = PgPool::new(env!("DB_URL")).await.unwrap();
+
     util::tracing::start_tracing();
     let routes = routes::routes().with(warp::trace::request());
     util::starter::start_server(routes).await;
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
 use warp::{Filter, Rejection, Reply};
 
 pub fn routes() -> impl Filter<Extract = (impl Reply,), Error= Rejection> + Clone + Send + Sync + 'static {
+    warp::path!("hello" / String).map(|name| format!("Hello {}!",name))
 }
